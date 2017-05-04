6d0d3b9d0930c31900139c33520074b333d61c52
hanspeterka <janmpeterka@gmail.com>
2017-05-04 11:26:26 +0200

    Základ karet

--- game.js
+++ game.js
@@ -1,37 +1,89 @@
-function main () {
-	var canvas = getEl('c');
-	var ctx = canvas.getContext('2d');
-	var cubes = []; 
+var cardCount = 0
 
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight - 150;
+function main() {
+    var canvas = getEl('c');
+    var ctx = canvas.getContext('2d');
+    var cubes = [];
 
-	for (var i = 0; i < 1; i++) {
-		new Cube();
-	};
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 150;
 
-	function Cube () {
-		this.a = 50;
-		this.x = 150;
-		this.y = canvas.height - this.a;
-		this.draw = function(){
-			ctx.fillRect(this.x, this.y, this.a, this.a);
-		}
-		this.update = function() {
-			this.x++;
-		};
-		cubes.push(this);
-	}
+    for (var i = 0; i < 1; i++) {
+        new Cube();
+    };
 
-	function update () {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		for (var i = 0; i < cubes.length; i++) {
-			cubes[i].update();
-			cubes[i].draw();
-		};
-	}
-	setInterval(update, 20)
+    function Cube() {
+        this.a = 50;
+        this.x = 150;
+        this.y = canvas.height - this.a;
+        this.draw = function() {
+            ctx.fillRect(this.x, this.y, this.a, this.a);
+        }
+        this.update = function() {
+            this.x++;
+        };
+        cubes.push(this);
+    }
+
+    function update() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < cubes.length; i++) {
+            cubes[i].update();
+            cubes[i].draw();
+        };
+    }
+    setInterval(update, 20)
+
+
+
 }
-function getEl (el) {
-	return document.getElementById(el);
+
+
+//
+function getEl(el) {
+    return document.getElementById(el);
 }
+
+// Control panel
+
+function addCard() {
+    // alert("A-E")
+    var rand = Math.floor(Math.random() * 3)
+    console.log(rand)
+    var colors = ["red", "green", "blue"]
+
+    $("#cp-list").append($("<li class='card " + colors[rand] + "''></li>"))
+    cardCount++
+    // Not a nice solution WIP
+    $(".card").unbind("click");
+    $(".card").click(function() {
+        $(this).playCard()
+    })
+}
+
+
+$.fn.playCard = function playCard() {
+    score = score + 1
+    getEl("score").innerHTML = score;
+
+    $(this).removeCard();
+
+}
+
+$.fn.removeCard = function removeCard() {
+    cardCount--
+    $(this).remove();
+}
+
+function restart() {
+    score = 0
+    getEl("score").innerHTML = score;
+}
+setInterval(function() {
+    if (cardCount < 8) {
+        addCard()
+    }
+
+}, 1000)
+
+restart()
